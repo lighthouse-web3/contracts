7a0628a914691475159d3c725fa6e8260c5bb174
Nandit Mehra <nanditmehra123@gmail.com>
2022-01-09 01:44:06 +0530

    Update DepositManager.sol

--- contracts/DepositManager.sol
+++ contracts/DepositManager.sol
@@ -3,6 +3,13 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract DepositManager {
+    address owner = msg.sender;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     struct Deposit {
         uint timestamp;
         uint amount;
@@ -14,6 +21,8 @@ contract DepositManager {
         uint availableStorage;
     }
 
+    address[] public whitelistedAddresses;
+ 
     mapping (address => Deposit[]) public deposits;
     mapping (address => Storage) public storageUsed;
 
@@ -32,5 +41,29 @@ contract DepositManager {
         // top up storage against the deposit - above event emitted can be used in node
     }
 
-    
+    function addWhitelistAddress(address addr) public onlyOwner {
+        whitelistedAddresses.push(addr);
+    }
+
+    function removeWhitelistAddress(address addr) public onlyOwner {
+        uint index = 0;
+        for (index = 0; index < whitelistedAddresses.length; index++) {
+            if (whitelistedAddresses[index] == addr) {
+                break;
+            }
+        }
+
+        for (uint i = index; i<whitelistedAddresses.length-1; i++){
+            whitelistedAddresses[i] = whitelistedAddresses[i+1];
+        }
+        whitelistedAddresses.pop();
+    }
+
+    function listWhitelistAddresses() public view returns (address[] memory) {
+        address[] memory addressList = new address[](whitelistedAddresses.length);
+        for (uint256 i = 0; i < whitelistedAddresses.length; i++) {
+            addressList[i] = whitelistedAddresses[i];
+        }
+        return addressList;
+    }
 }
