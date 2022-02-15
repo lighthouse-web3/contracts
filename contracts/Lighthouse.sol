91ce93d6b141d50e5a8d98e2442ed2790035fbbc
Nandit Mehra <nanditmehra123@gmail.com>
2022-02-15 18:00:07 +0530

    Update Lighthouse.sol

--- contracts/Lighthouse.sol
+++ contracts/Lighthouse.sol
@@ -1,35 +1,34 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Lighthouse  {
-    address owner = msg.sender;
+    address public owner = msg.sender;
 
     struct Content {
         string cid;
         string config;
         uint fileCost;
+        string fileName;
+        uint fileSize;
+        uint timestamp;
     }
 
     struct Status {
         string dealIds;
         bool active;
-        // mapping (uint => string) miners;
     }
 
-    event StorageRequest(address uploader, string cid, string config, uint fileCost);
+    event StorageRequest(address indexed uploader, string cid, string config, uint fileCost, string fileName, uint fileSize, uint timestamp);
     event StorageStatusRequest(address requester, string cid);
 
-    mapping(address => mapping(string => Content)) public requests;
     mapping(string => Status) public statuses; // address -> cid -> status
 
-    function store(string calldata cid, string calldata config)
+    function store(string calldata cid, string calldata config, string calldata fileName, uint fileSize)
         external
         payable
     {
-        uint fileCost = msg.value;
-        requests[msg.sender][cid] = Content(cid, config, fileCost);
-        emit StorageRequest(msg.sender, cid, config, msg.value);
+        uint currentTime = block.timestamp;
+        emit StorageRequest(msg.sender, cid, config, msg.value, fileName, fileSize, currentTime);
     }
 
     function getPaid(uint amount, address payable recipient)
@@ -47,7 +46,7 @@ contract Lighthouse  {
 
     function publishStorageStatus(string calldata cid, string calldata dealIds, bool active) 
         external
-    { // restrict it to only to the user
+    {   // restrict it to only to the owner address
         require(msg.sender == owner);
         statuses[cid] = Status(dealIds, active);
     }
