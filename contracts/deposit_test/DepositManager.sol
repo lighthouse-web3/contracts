21ee54474126b22615677f3100e4e75d3842f251
amarnath-arch <amarnath.4147ac@gmail.com>
2022-04-22 23:15:15 +0530

    linting and naming conventions updates--->Deposit manager

--- contracts/deposit_test/DepositManager.sol
+++ contracts/deposit_test/DepositManager.sol
@@ -13,11 +13,11 @@ contract DepositManager {
     address public manager = msg.sender;
     uint256 public costOfStorage = 5; // 5$/GB
 
-    string[] public stableCoins;
+    string[] public stableCoinsList;
 
     mapping(address => Deposit[]) public deposits;
     mapping(address => Storage) public storageList;
-    mapping (string => address) public stableCoin;
+    mapping (string => address) public stableCoins;
 
     struct Deposit {
         uint256 timestamp;
@@ -41,26 +41,17 @@ contract DepositManager {
         uint256 storagePurchased
     );
   
-    // go through this ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
-    // handle stablecoin payments
-
-    // ###################################
-    // update according to three coins
-    // mapping (symbol => addresses) function 
-    // add manager 
     
     
 
     function addDeposit(string calldata  _coinSymbol) public payable {
         address wallet = msg.sender;
 
-        require(IERC20(stableCoin[_coinSymbol]).balanceOf(wallet) > 0, "Must include deposit > 0");
-        uint256 storagePurchased = (IERC20(stableCoin[_coinSymbol]).balanceOf(wallet)).div(costOfStorage); // @todo work on this part    -----// can lead to integer division.
-        // ****************************************************************
+        require(IERC20(stableCoins[_coinSymbol]).balanceOf(wallet) > 0, "Must include deposit > 0");
+        uint256 storagePurchased = (IERC20(stableCoins[_coinSymbol]).balanceOf(wallet)).div(costOfStorage);
+ 
         deposits[msg.sender].push(Deposit(block.timestamp, msg.value, storagePurchased));
 
-        ///*********************
-        // update available storage
         updateAvailableStorage(msg.sender, storagePurchased);
 
         emit AddDeposit(msg.sender, msg.value, storagePurchased);
@@ -72,49 +63,54 @@ contract DepositManager {
         costOfStorage = newCost;
     }
 
-    // create function for transfer onlyOwner()********
+  
 
     function compareStrings(string memory a, string memory b) public view returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
 
 
-    function transferAmount(string memory _coinSymbol, address wallet, uint256 amount) external onlyOwner{
+    function transferAmount(
+        string memory _coinSymbol, 
+        address wallet, 
+        uint256 amount
+    ) external onlyOwner {
 
-        for(uint256 i=0;i < stableCoins.length; i++){
-            string memory coin= stableCoins[i];
-            if(compareStrings(coin, _coinSymbol)){
-                require(amount <= IERC20(stableCoin[_coinSymbol]).balanceOf(address(this)));
-                IERC20(stableCoin[_coinSymbol]).transfer(wallet,amount);
+        for(uint256 i=0 ; i < stableCoinsList.length; i++ ) {
+            string memory coin= stableCoinsList[i];
+            if(compareStrings(coin, _coinSymbol)) {
+                require(amount <= IERC20(stableCoins[_coinSymbol]).balanceOf(address(this)));
+                IERC20(stableCoins[_coinSymbol]).transfer(wallet, amount);
             }
         }
 
     }
 
-    function addCoin(string calldata _coinSymbol, address _coinAddress) external onlyOwner{
-        require(stableCoin[_coinSymbol] == address(0),"Coin Already Added");
-        stableCoins.push(_coinSymbol);
-        stableCoin[_coinSymbol] = _coinAddress;
+    // adding and removing available stablecoins on the contract.
+
+    function addCoin(
+        string calldata _coinSymbol, 
+        address _coinAddress
+    ) external onlyOwner {
+        require(stableCoins[_coinSymbol] == address(0), "Coin Already Added");
+        stableCoinsList.push(_coinSymbol);
+        stableCoins[_coinSymbol] = _coinAddress;
     } 
 
-    function removeCoin(string calldata _coinSymbol)external onlyOwner{
-        require(stableCoin[_coinSymbol] != address(0), "Coin Already Removed or doesn't exist");
-        stableCoin[_coinSymbol] = address(0);
+
+    function removeCoin(string calldata _coinSymbol)external onlyOwner {
+        require(stableCoins[_coinSymbol] != address(0), "Coin Already Removed or doesn't exist");
+        stableCoins[_coinSymbol] = address(0);
     }
 
 
-
-
-    // why not use the concept of whitelist addresses???? 
-    // it can be onlyowner or the lighthouse contract.
-    // test with msg.sender
-    // create different function for bundle store (user)
+    // update storage 
 
     function updateStorage(
         address user,
         uint256 filesize,
         string calldata cid
-    ) public onlyManager {
+    ) public ManagerorOwner {
         storageList[user].cids.push(cid);
         storageList[user].totalStored = storageList[user].totalStored.add(filesize);
         storageList[user].availableStorage = storageList[user].availableStorage.sub(filesize);
@@ -124,18 +120,18 @@ contract DepositManager {
     function updateAvailableStorage(
         address user,
         uint256 addOnStorage
-    ) public onlyManager {
+    ) public ManagerorOwner {
         Storage memory storageUpdate;
         storageUpdate.availableStorage = storageList[user].availableStorage.add(addOnStorage);
         storageUpdate.totalStored = storageList[user].totalStored;
         storageList[user] = storageUpdate;
     }
 
-    function changeOwner(address _owner) external onlyOwner{
+    function changeOwner(address _owner) external onlyOwner {
         owner = _owner;
     }
 
-    function changeManager(address _manager)external onlyManager{
+    function changeManager(address _manager)external ManagerorOwner {
         manager = _manager;
     }
 
@@ -145,8 +141,8 @@ contract DepositManager {
         _;
     }
 
-    modifier onlyManager(){
-        require((msg.sender == manager) || (msg.sender == owner),"Not Authorised!!!");
+    modifier ManagerorOwner() {
+        require((msg.sender == manager) || (msg.sender == owner), "Not Authorised!!!");
         _;
     }
 
