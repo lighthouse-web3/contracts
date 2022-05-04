4c1527b5399d9ecec7e0735c8c1e03e46e1ffc32
xlassix <46370698+xlassix@users.noreply.github.com>
2022-05-04 09:49:08 +0100

    fix: DepositManager.sol

--- contracts/stablecoins/dai.sol
+++ contracts/stablecoins/dai.sol
@@ -5,9 +5,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Dai is ERC20 {
+
     constructor() ERC20("Dai Stablecoin", "DAI") {}
 
     function faucet(address recipient, uint256 amount) external {
         _mint(recipient, amount);
     }
+
 }
